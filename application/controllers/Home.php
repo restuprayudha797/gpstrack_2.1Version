53735c7d3e97078f8338ee5a6c6bc65c0ff42f0d
restuprayudha797 <webmerdeka2911@gmail.com>
2023-10-25 21:13:53 +0700

    added comment

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,21 +1,18 @@
-<?php 
-defined('BASEPATH') OR exit('No direct script access allowed');
-class Home extends CI_Controller{
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+class Home extends CI_Controller
+{
 
 
-    public function index(){
+    public function index()
+    {
 
 
-    $data['title'] = 'Beranda';
+        $data['title'] = 'Beranda';
 
-    $this->load->view('frontend/layout/frontend-header', $data);
-    $this->load->view('frontend/layout/frontend-navbar');
-    $this->load->view('frontend/home/index-home');
-    $this->load->view('frontend/layout/frontend-footer');
-
-
+        $this->load->view('frontend/layout/frontend-header', $data);
+        $this->load->view('frontend/layout/frontend-navbar');
+        $this->load->view('frontend/home/index-home');
+        $this->load->view('frontend/layout/frontend-footer');
     }
-
-
-
 }
